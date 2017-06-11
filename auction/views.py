5c13da099a1ce3b5ba7b5441bd8cf8b72a4f34c9
WitchZone <diedrakon@yandex.ru>
2017-06-11 14:19:37 +0500

    post from db

--- auction/views.py
+++ auction/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Lot_sub
 
 # Create your views here.
 
 def lot_list(request):
-    return render(request, 'auction/index.html', {})
+    lots = Lot_sub.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'auction/index.html', {'lots': lots})
